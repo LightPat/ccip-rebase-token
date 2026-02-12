2174b4b2dce0d02bf5c9857fa41d72923e565d3e
Patrick Seeman <patseem189@gmail.com>
2026-02-12 14:24:59 -0500

    transfer and transfer from

--- src/RebaseToken.sol
+++ src/RebaseToken.sol
@@ -84,6 +84,44 @@ contract RebaseToken is ERC20, Ownable, AccessControl {
     }
 
     /**
+     * Transfer tokens from one user to another
+     * @param _recipient The user to transfer the tokens to
+     * @param _amount The amount of tokens to transfer
+     * @return true if the transfer was successful, false otherwise
+     */
+    function transfer(address _recipient, uint256 _amount) public override returns (bool) {
+        _mintAccruedInterest(msg.sender);
+        _mintAccruedInterest(_recipient);
+        if (_amount == type(uint256).max) {
+            _amount = balanceOf(msg.sender);
+        }
+        if (balanceOf(_recipient) == 0) {
+            s_userInterestRate[_recipient] = s_userInterestRate[msg.sender];
+        }
+        return super.transfer(_recipient, _amount);
+    }
+
+    /**
+     * Transfer tokens from one user to another
+     * @param _sender The user to transfer the tokens from
+     * @param _recipient The user to transfer the tokens to
+     * @param _amount The amount of tokens to transfer
+     * @return true if the transfer was successful, false otherwise
+     */
+    function transferFrom(address _sender, address _recipient, uint256 _amount) public override returns (bool) {
+        _mintAccruedInterest(_sender);
+        _mintAccruedInterest(_recipient);
+        if (_amount == type(uint256).max) {
+            _amount = balanceOf(_sender);
+        }
+        if (balanceOf(_recipient) == 0) {
+            s_userInterestRate[_recipient] = s_userInterestRate[_sender];
+        }
+        return super.transferFrom(_sender, _recipient, _amount);
+
+    }
+
+    /**
      * @dev returns the interest accrued since the last update of the user's balance - aka since the last time the interest accrued was minted to the user.
      * @return linearInterest the interest accrued since the last update
      *
