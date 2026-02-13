8ac0780f9be16a57378e2a2b7375552970779d8c
Patrick Seeman <patseem189@gmail.com>
2026-02-13 12:02:33 -0500

    vault contract

---
+++ src/Vault.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {IRebaseToken} from "./interfaces/IRebaseToken.sol";
+
+contract Vault {
+    error Vault__RedeemFailed();
+
+    event Deposit(address indexed user, uint256 amount);
+    event Redeem(address indexed user, uint256 amount);
+
+    IRebaseToken private immutable i_rebaseToken;
+
+    constructor(IRebaseToken _rebaseToken) {
+        i_rebaseToken = _rebaseToken;
+    }
+
+    receive() external payable {
+
+    }
+
+    /**
+     * @notice Allows users to deposit ETH into the vault and mint rebase tokens in return
+     */
+    function deposit() external payable {
+        i_rebaseToken.mint(msg.sender, msg.value);
+        emit Deposit(msg.sender, msg.value);
+    }
+
+    /**
+     * @notice Allows users to redeem their rebase tokens for ETH
+     * @param _amount The amount of rebase tokens to redeem
+     */
+    function redeem(uint256 _amount) external {
+        i_rebaseToken.burn(msg.sender, _amount);
+        (bool success, ) = msg.sender.call{value: _amount}("");
+        if (!success) {
+            revert Vault__RedeemFailed();
+        }
+        emit Redeem(msg.sender, _amount);
+    }
+
+    /**
+     * @notice Get the address of the rebase token
+     * @return The address of the rebase token
+     */
+    function getRebaseTokenAddress() external view returns (address) {
+        return address(i_rebaseToken);
+    }
+}
